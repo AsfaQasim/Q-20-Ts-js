719ff0baa7ba2cb4ae99189bce183257cb312443
Asfa Qasim <160766420+AsfaQasim@users.noreply.github.com>
2024-03-08 07:55:23 -0800

    Q 20 of Ts/js & node js of 45 assignment.
    Q 20 of Ts/js & node js of 45 assignment.

---
+++ main.js
@@ -0,0 +1,6 @@
+var languages = ["English", "Urdu", "Chinese", "French"];
+console.log("List of languages:");
+for (var _i = 0, languages_1 = languages; _i < languages_1.length; _i++) {
+    var top_1 = languages_1[_i];
+    console.log(top_1);
+}
---
+++ main.ts
@@ -0,0 +1,5 @@
+let languages : string [] =["English","Urdu","Chinese","French"];
+console.log("List of languages:");
+for(let top of languages){
+    console.log(top);
+}
